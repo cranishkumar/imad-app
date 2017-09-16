2660f9309c31efa96b6507dad888c60c35c090db
cranishkumar <31035727+cranishkumar@users.noreply.github.com>
2017-09-16 13:39:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,3 +4,10 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'New Value';
+//Move the image littele right
+
+var img = document.getElementById('img');
+
+img.onclick = function(){
+    var interval = setInterval(mioveLeft, 100);
+};
