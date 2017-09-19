7e58d18a4d5aff3faa435f7236a4c99af5684ea2
cranishkumar <31035727+cranishkumar@users.noreply.github.com>
2017-09-19 23:48:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,32 +17,6 @@ img.onclick = function() {
 };
 */
 
-//counter code
-
-var counter = 0;
-var button = document.getElementById('counter');
-
-button.onclick = function(){
-
-    //create a request object
-    var request = new XMLHttpRequest();
-
-    //Capture the response and store it in a variable
-    request.onreadystatechange = function () {
-     if (request.readyState === XMLHttpRequest.DONE) {
-         //Take some action
-         if (request.status === 200) {
-            var counter = request.responseText;
-            //Render the request result in the correct span
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
-         }
-     } 
-    };
-    //Make a request
-    request.open('GET','http://cranishkumar.imad.hasura-app.io/counter', true);
-    request.send(null);
-};
 
 
 var submit = document.getElementById('submit_btn');
