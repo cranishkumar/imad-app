ce33a2e0132647663cc497dd78e0c1a875ac16c2
cranishkumar <31035727+cranishkumar@users.noreply.github.com>
2017-09-20 23:40:37 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -175,7 +175,7 @@ app.get('/check-login', function (req, res) {
     }
 });
 
-app.get('/Logout', function (req, res) {
+app.get('/logout', function (req, res) {
     delete req.session.auth;//delete the session object
     res.send("Logged out");
 });
