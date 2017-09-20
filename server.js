6ad677c5350f4b1af6c9808d289babae54f7385e
cranishkumar <31035727+cranishkumar@users.noreply.github.com>
2017-09-20 23:17:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -145,7 +145,7 @@ app.post('/login', function (req, res) {
           res.status(500).send(err.toString());
       } else {
           if (result.rows.length === 0) {
-              res.send(403).send('username/password is invaalid');
+              res.send(403).send('username/password is invalid');
           } else {
               //Match the password
               var dbstring = result.rows[0].password;
